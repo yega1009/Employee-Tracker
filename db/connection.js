d8ae49b9fd90c8e11983dae3f5590a1095c69504
Yega Cho <yega1009@gmail.com>
2023-10-07 14:31:55 -0400

    imported dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
-const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Lion2001^^',
-    database: 'employee_tracker'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection;
