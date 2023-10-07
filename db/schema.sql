829faa2fec41ca47e1f7230345ba9984e6452c78
Yega Cho <yega1009@gmail.com>
2023-10-07 17:48:54 -0400

    added comments for schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,19 @@
+-- Drop the database if it already exists
 DROP DATABASE IF EXISTS employee_tracker;
+
+-- Creat a new database
 CREATE DATABASE employee_tracker;
 
+-- Select the database to execute queries
 USE employee_tracker;
 
+-- Create a 'department' table with 'id' and 'name'
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
+-- Creating a 'role' table with 'id', 'title', 'salary', and 'department_id' as a foreign key that references the 'id' of the 'department' table
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
@@ -16,6 +22,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
+-- Creating an 'employee' table with 'id', 'first_name', 'last_name', 'role_id' as a foreign key that references the 'id' of the 'role' table,
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
