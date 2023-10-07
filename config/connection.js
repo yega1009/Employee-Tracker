b3bf428f2de846a32efea51076d60f96faab79c4
Yega Cho <yega1009@gmail.com>
2023-10-07 17:43:59 -0400

    added comments for connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
+// Import the 'dotenv' library to load environment variables from a .env file into process.env
 require('dotenv').config();
 
+// Import the 'mysql2' library to create a MySQL connection
 const mysql = require('mysql2');
 
+// Create a connection to the MySQL database using the configuration from the environment variables
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
+    host: 'localhost', 
+    user: process.env.DB_USER, 
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME 
 });
 
+// Export the connection object to be used in other parts of the application
 module.exports = connection;
